64a6d6b865e2416fb801ec4cd32c19c33f6ec11d
Matheus Holanda <matheusnh6@gmail.com>
2026-07-24 19:54:34 -0300

    OrderStatus enum

--- src/main/java/com/aprendendospring/course/entities/Order.java
+++ src/main/java/com/aprendendospring/course/entities/Order.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.JoinColumn;
 import java.io.Serializable;
+import com.aprendendospring.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +23,9 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -29,10 +33,11 @@ public class Order implements Serializable {
     public Order() {
     }
     
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -49,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+    if (orderStatus != null) {
+        this.orderStatus = orderStatus.getCode();
+    }
     }
 
     public User getClient() {
