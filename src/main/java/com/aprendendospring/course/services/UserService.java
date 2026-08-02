03042516b25aa444ee982404216b54972d6511fe
Matheus Holanda <matheusnh6@gmail.com>
2026-08-02 16:54:37 -0300

    Exception handling - findById

--- src/main/java/com/aprendendospring/course/services/UserService.java
+++ src/main/java/com/aprendendospring/course/services/UserService.java
@@ -6,6 +6,7 @@ import com.aprendendospring.course.entities.User;
 import com.aprendendospring.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.aprendendospring.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);;
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // orElseThrow é para lançar uma exceção caso o objeto não seja encontrado
     }
     
     public User insert(User obj) {
@@ -37,10 +38,10 @@ public class UserService {
         User entity = repository.getReferenceById(id); // getReferenceById é para buscar o objeto no banco de dados
         updateData(entity, obj); // updateData é para atualizar os dados do objeto, entity é o objeto que está no banco de dados e obj é o objeto que veio da requisição
         return repository.save(entity); // repository.save é para salvar o objeto no banco de dados
-   
-        
     }
 
+        
+    
     private void updateData(User entity, User obj) { // updateData é para atualizar os dados do objeto, entity é o objeto que está no banco de dados e obj é o objeto que veio da requisição
         entity.setName(obj.getName()); // setName é para atualizar o nome do objeto
         entity.setEmail(obj.getEmail()); // setEmail é para atualizar o email do objeto
